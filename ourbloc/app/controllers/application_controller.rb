98d62e6677627edbda1cdc9d3b7738edf09c126d
Amina <amina@ourbloc.co>
2016-09-25 18:45:14 +0000

    changed devise config settings to allow changes to sign up/login forms

--- ourbloc/app/controllers/application_controller.rb
+++ ourbloc/app/controllers/application_controller.rb
@@ -1,46 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
   
+  BLOCS = %w[TheArts Business STEM/Tech ]
   
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:sign_up) << :school
-    devise_parameter_sanitizer.for(:sign_up) << :major
-    devise_parameter_sanitizer.for(:sign_up) << :experience
-    devise_parameter_sanitizer.for(:sign_up) << :bloc1
-    devise_parameter_sanitizer.for(:sign_up) << :bloc2
-    devise_parameter_sanitizer.for(:sign_up) << :bloc3
-    #devise_parameter_sanitizer.for(:sign_up) << :role
     
-    
-    devise_parameter_sanitizer.for(:account_update) << :school
-    devise_parameter_sanitizer.for(:account_update) << :major
-    devise_parameter_sanitizer.for(:account_update) << :experience
-    devise_parameter_sanitizer.for(:account_update) << :name 
-    devise_parameter_sanitizer.for(:account_update) << :image
-    devise_parameter_sanitizer.for(:account_update) << :skills
-    devise_parameter_sanitizer.for(:account_update) << :gradyear
-    devise_parameter_sanitizer.for(:account_update) << :orgs
-    devise_parameter_sanitizer.for(:account_update) << :links
-    devise_parameter_sanitizer.for(:account_update) << :threeskills
-    devise_parameter_sanitizer.for(:account_update) << :phone
-    devise_parameter_sanitizer.for(:account_update) << :resumelink
-    devise_parameter_sanitizer.for(:account_update) << :geo
-    devise_parameter_sanitizer.for(:account_update) << :hometown
-    devise_parameter_sanitizer.for(:account_update) << :company
-    devise_parameter_sanitizer.for(:account_update) << :mission
-    devise_parameter_sanitizer.for(:account_update) << :industry
-    devise_parameter_sanitizer.for(:account_update) << :statement
-    devise_parameter_sanitizer.for(:account_update) << :title
-    devise_parameter_sanitizer.for(:account_update) << :role
-    devise_parameter_sanitizer.for(:account_update) << :resume
-    
-    devise_parameter_sanitizer.for(:account_update) << :bloc1
-    devise_parameter_sanitizer.for(:account_update) << :bloc2
-    devise_parameter_sanitizer.for(:account_update) << :bloc3
-    devise_parameter_sanitizer.for(:account_update) << :bloc4
-    
-
+  
+    added_registration_attrs = [:name, :school, :major, :experience, :bloc1, :bloc2, :bloc3 ]
+    added_profile_attrs = [:name, :school, :major, :experience, :bloc1, :bloc2, :bloc3, :skills, :gradyear, :orgs, :resumelink, :geo ,:hometown ,:company]
+  
+    devise_parameter_sanitizer.permit :sign_up, keys: added_registration_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_profile_attrs
   end
+  
+
+
 end
