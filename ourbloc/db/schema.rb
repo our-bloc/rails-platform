f115a94decf92f4d5337ded848cddc23ece12fdd
Amina <amina@ourbloc.co>
2016-09-26 03:14:39 +0000

    add roles column to Users, predefined roles already set in user model

--- ourbloc/db/schema.rb
+++ ourbloc/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926011256) do
+ActiveRecord::Schema.define(version: 20160926031248) do
 
   create_table "asks", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 20160926011256) do
     t.string   "statement"
     t.string   "title"
     t.string   "resumelink"
+    t.string   "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
