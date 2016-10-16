e05604e1b8600bd1a1006c70f26157a32702c7e3
Amina <amina@ourbloc.co>
2016-10-16 03:20:46 +0000

    admin users seeded

--- ourbloc/db/seeds.rb
+++ ourbloc/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+admins= User.create ([{name: "Amina Yamusah", email: "amina@ourbloc.co", password: "aminabloc"}, 
+{name: "Bridgette Depay", email: "about@depay.info", password: "bridgettebloc"},
+{name: "Alivia Blount", email: "aliviablount@gmail.com", password: "aliviabloc"}])
+
