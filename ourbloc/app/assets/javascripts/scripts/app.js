ec6dd661a906b9c651e1c89a2fbbeead2fa5df1f
Alivia Blount <aliviablount@gmail.com>
2016-10-17 12:53:20 -0700

    Edit welcome#home view with tabs, edit assets, ap.js, css/style.css

--- ourbloc/app/assets/javascripts/scripts/app.js
+++ ourbloc/app/assets/javascripts/scripts/app.js
@@ -1,22 +1,15 @@
-console.log('Sanity Check: app.js is linked correctly!');
+$(document).ready(function () {
+  $(".tabs a").on("click", function(e) {
+    e.preventDefault();
 
-$(document).ready(function(){
+    $(this).parent().addClass("active");
+    $(this).parent().siblings().removeClass("active");
+    var tab = $(this).attr("href");
 
-  // select a template script, and pull out the handlebars and html inside it
-  var source = $('#department-list-template').html();
-  console.log('template script source:', source);
+    $("tab-content").not(tab).css("display", none);
+    $(tab).fadeIn();
 
-  // compile the handlebars template
-  var template = Handlebars.compile(source);
-
-  // use the template function from handlebars to create an HTML string
-  // the template function takes in an object where:
-    // each key is a variable the html template expects
-    // each key's value is the data we want that variable to have
-  var departmentHtml = template({ department: data.departments });
-  console.log('generated html string:', departmentHtml);
-
-  // append html to the view
-  $("#department-list").append(departmentHtml);
-
+    $("nested-tab-content").not(tab).css("display", none);
+    $(tab).fadeIn();
+  });
 });
