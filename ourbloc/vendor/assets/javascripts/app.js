aa15fbc8feb6c539342aaf8911f19f0eed75755e
Alivia Blount <aliviablount@gmail.com>
2016-10-18 13:06:04 -0700

    clean up asset pipeline, delete unused files

---
+++ ourbloc/vendor/assets/javascripts/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $('#tabs').tabs();
+  // $(".tabs a").on("click", function(e) {
+  //   e.preventDefault();
+  //
+  //   $(this).parent().addClass("active");
+  //   $(this).parent().siblings().removeClass("active");
+  //   var tab = $(this).attr("href");
+  //
+  //   $("tab-content").not(tab).css("display", none);
+  //   $(tab).fadeIn();
+  //
+  //   $("nested-tab-content").not(tab).css("display", none);
+  //   $(tab).fadeIn();
+  // });
+});
