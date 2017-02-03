4b9268cc6c1241e6b4cb31ac793866d1acda129c
Amina <amina@ourbloc.co>
2017-02-03 00:25:16 +0000

    welcome controller loop

--- ourbloc/app/controllers/welcome_controller.rb
+++ ourbloc/app/controllers/welcome_controller.rb
@@ -1,12 +1,15 @@
 class WelcomeController < ApplicationController
   def home
-    @vision = Vision.where(:id => current_user.uid)
-    if user_signed_in? and @vision = nil
-      redirect_to new_vision_path
-    elsif user_signed_in?
-      redirect_to myprofile_path
+    if user_signed_in?
+      @vision = Vision.where(:id => current_user.uid)
+          if user_signed_in? and @vision = nil
+            redirect_to new_vision_path
+          else user_signed_in?
+            redirect_to myprofile_path
+          end
     else
       redirect_to new_user_registration_path
     end
+    
   end
 end
