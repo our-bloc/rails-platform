43f166a151bc34ba8bf4191c9bf7bf26e249ee7a
Amina <amina@ourbloc.co>
2017-02-07 23:08:44 +0000

    prepping seed file for deployment

--- ourbloc/db/seeds.rb
+++ ourbloc/db/seeds.rb
@@ -1,48 +1,13 @@
-User.delete_all
-Sector.delete_all
 Share.delete_all
 Ask.delete_all
 Job.delete_all
 Resource.delete_all
-
-User.create ([
-  {name: "Amina Yamusah", email: "amina@ourbloc.co", password: "aminabloc"},
-  {name: "Bridgette Depay", email: "about@depay.info", password: "bridgettebloc"},
-  {name: "Alivia Blount", email: "aliviablount@gmail.com", password: "aliviabloc"}
-])
+Influencer.delete_all
+Tip.delete_all
 
 
-sectors= [
-  "Academia",
-  "Arts",
-  "Advertising, Sales, Marketing",
-  "Business",
-  "Entreprenuership",
-  "Finance & Consulting",
-  "Media & Entertainment",
-  "Law & Government",
-  "Social Impact",
-  "STEM",
-  "Tech",
-  "Venture Capital & Philanthropy",
-  "Talent & Diversity"
-]
 
-# creating all of the categorys
-sectors.each do |s|
-  category = Sector.create(name: s) #don't save yet, we need to add more info
 
-  1.times do
-    Resource.create ({user_id: 1, sector_id: category.id, title: "Check out this dope guide for applying to grad school", text: "AWP's Guide to Writing Programs is a free, searchable database of graduate and undergraduate writing programs in the US and abroad. Each member program listing contains information about faculty, admissions, requirements of study, and tuition and financial aid.
-Nonmember programs can enhance their listings by joining AWP today.
-For additional resources to help you in your search for writing programs, please read our program advice articles in the Writer's Notebook blog.
-Use the search and browse features below to explore undergraduate and graduate writing programs.
-"})
-    Resource.create ({user_id: 2, sector_id: category.id, title: "Portofolios are the perfect way to show off your writing & increase your app success", text: "Check out this guide https://blog.quiet.ly/community/how-to-create-a-writing-portfolio-for-freelancers/"})
-    Resource.create ({user_id: 3, sector_id: category.id, title: "Here are some of our favorite jobs board for creatives & writers text", text: "Create Daily , The Muse , Idealist"})
-  end
-
-end
 
 
 
@@ -88,10 +53,10 @@ Influencer.create ([
   {name: "Rodney Simpson", industry:"Entrepreneurs", style: "Earn", description: " "}, 
   {name: "Chris Gray", industry:"Entrepreneurs", style: "Jackson", description: " "},
   
-  {name: "Wondaland", industry:"Artists", style: "Olivia", description: " "},
-  {name: "Solange", industry:"Artists", style: "Issa", description: " "}, 
-  {name: "Kevin Beasley", industry:"Artists", style: "Earn", description: " "}, 
-  {name: "Misty Copeland", industry:"Artists", style: "Jackson", description: " "},
+  {name: "Wondaland", industry:"Creatives", style: "Olivia", description: " "},
+  {name: "Solange", industry:"Creatives", style: "Issa", description: " "}, 
+  {name: "Kevin Beasley", industry:"Creatives", style: "Earn", description: " "}, 
+  {name: "Misty Copeland", industry:"Creatives", style: "Jackson", description: " "},
 
 ])
 
