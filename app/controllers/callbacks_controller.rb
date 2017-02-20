f2d01751e7f03b4fc0fb0cc43b7dd193dcfe177f
Amina <amina@ourbloc.co>
2017-02-20 17:52:30 +0000

    fixing callback error when referrer address is nil

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -10,9 +10,14 @@ class CallbacksController < Devise::OmniauthCallbacksController
  
         
         sign_in_and_redirect @user
-        code= request.referer.split('/')[-1]
-        current_user.update_attributes(:referral_code => code)
         
+        if request.referer == nil
+            code = 
+            current_user.update_attributes(:referral_code => code)
+        else
+            code= request.referer.split('/')[-1]
+            current_user.update_attributes(:referral_code => code)
+        end
     end
     
 end
