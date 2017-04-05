74c097cc7284dce0ba36b0b09f12953a3e63d8a9
Amina <amina@ourbloc.co>
2017-04-05 02:06:37 +0000

    welcome email after completing quiz

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -2,7 +2,7 @@ class WelcomeMailer < ApplicationMailer
     default from: "amina@ourbloc.co"
     
     def welcome_email(user)
-        @user = user
+        @user = current_user
         mail(to: @user.email, subject: 'Hey ' + @user.firstname + ', Welcome to Bloc!')
-     end
+    end
 end
