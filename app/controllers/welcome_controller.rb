82b32c6febb3f93461caf2f37f6e09b5df7f4c85
Amina <amina@ourbloc.co>
2017-04-05 00:48:09 +0000

    fixing welcome controller bug, only redirects to new vision on first login

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,13 +4,15 @@ class WelcomeController < ApplicationController
   def home
     
     @user = current_user
-    if user_signed_in?
+    if user_signed_in? 
       @vision = Vision.all.where(:user_id == @user.id)
     end
   
-    if user_signed_in?
+    if user_signed_in? and current_user.sign_in_count == 1
       redirect_to new_vision_path
-    else
+    elsif user_signed_in? and current_user.sign_in_count >= 1
+      redirect_to myprofile_path 
+    else 
       redirect_to new_user_registration_path
     end
     
