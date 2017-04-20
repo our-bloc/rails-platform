ad58a63badf8281e358c245e770d2461423573f8
Amina <amina@ourbloc.co>
2017-04-20 18:14:48 +0000

    fixing user paths for people using old login and new login flows

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,13 +8,19 @@ class WelcomeController < ApplicationController
       @vision = Vision.all.where(:user_id == @user.id)
     end
   
-    if user_signed_in? and user.industry != nil
-      redirect_to playlist_path
-    else 
-      redirect_to new_user_registration_path
-    end
     
-  end
+    if !user_signed_in?
+      redirect_to new_user_registration_path
+      
+    elsif user_signed_in? and current_user.sign_in_count == 1
+      redirect_to playlist_path
+      
+    elsif user_signed_in? and current_user.sign_in_count > 1 and user.industry == nil
+      redirect_to myprofile_path 
+      
+    else user_signed_in? and current_user.sign_in_count > 1 and user.industry != nil
+      redirect_to playlist_path
+    end
   
   def landing 
    
