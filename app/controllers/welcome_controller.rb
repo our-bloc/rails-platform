b46e97c8cfe21b46a28c0d24ae8e9ee8a7f08bdc
Amina <amina@ourbloc.co>
2017-04-21 20:25:19 +0000

    fb users can login without issues

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,11 +12,15 @@ class WelcomeController < ApplicationController
     if !user_signed_in?
       redirect_to new_user_registration_path
       
-    elsif user_signed_in? and @vision == nil
+    elsif (user_signed_in? and current_user.industry != nil)
       redirect_to playlist_path
+    
+    elsif (user_signed_in? and current_user.sign_in_count == 1 and current_user.industry == nil)
+      redirect_to new_vision_path
       
-    elsif user_signed_in? and @vision != nil
-      redirect_to vision_path(@vision)
+    elsif (user_signed_in? and  current_user.industry == nil and current_user.sign_in_count > 1 )
+      redirect_to myprofile_path 
+
     end
   end
   
