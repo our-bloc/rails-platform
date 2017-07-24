1a53bb48677c389804d2265815631a437e5ecf3b
root <amina@ourbloc.co>
2017-07-24 09:25:05 -0400

    pushing to master after merge with con registration

--- db/migrate/20170714203801_add_users_to_events_and_rsvps.rb
+++ db/migrate/20170714203801_add_users_to_events_and_rsvps.rb
@@ -3,7 +3,6 @@ class AddUsersToEventsAndRsvps < ActiveRecord::Migration[5.0]
      add_column :rsvps, :user_id, :integer
     add_index :rsvps, :user_id 
     
-     add_column :events, :user_id, :integer
-    add_index :events, :user_id 
+    
   end
 end
--- db/migrate/20170719150423_add_user_id_to_rsvps.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToRsvps < ActiveRecord::Migration[5.0]
-  def change
-    add_column :rsvps, :user_id, :integer
-    add_index :rsvps, :user_id 
-  end
-end
